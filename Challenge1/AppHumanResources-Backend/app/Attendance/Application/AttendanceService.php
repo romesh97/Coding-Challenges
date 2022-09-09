82fcb9d4d513867c70f80a2f9df76f1b794eadd4
Romesh <romeshperera97@gmail.com>
2022-09-09 11:16:51 +0530

    upload file method body added

--- Challenge1/AppHumanResources-Backend/app/Attendance/Application/AttendanceService.php
+++ Challenge1/AppHumanResources-Backend/app/Attendance/Application/AttendanceService.php
@@ -2,10 +2,17 @@
 
 namespace App\Attendance\Application;
 
+use App\Models\Attendance\Domain\Attendance;
+use Maatwebsite\Excel\Facades\Excel;
+
 class AttendanceService
 {
-    public static function uploadFile()
+    //UPLOAD CSV FILE FUNCTION
+    public function uploadFile(Request $request)
     {
+        Excel::import(new AttendanceImport, $request->file);
+
+        return ('User Imported Successfully');
 
     }
 }
