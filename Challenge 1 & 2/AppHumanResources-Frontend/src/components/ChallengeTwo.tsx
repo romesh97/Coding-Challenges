f21ba0c068be58db1e49734a188081c8ac865570
Romesh <romeshperera97@gmail.com>
2022-09-13 02:16:43 +0530

    operator modifications

--- Challenge 1 & 2/AppHumanResources-Frontend/src/components/ChallengeTwo.tsx
+++ Challenge 1 & 2/AppHumanResources-Frontend/src/components/ChallengeTwo.tsx
@@ -12,7 +12,7 @@ export default function ChallengeTwo() {
 
     let results = [];
     for (let i = 0; i < sorted_arr.length - 1; i++) {
-      if (sorted_arr[i + 1] == sorted_arr[i]) {
+      if (sorted_arr[i + 1] === sorted_arr[i]) {
         results.push(sorted_arr[i]);
       }
     }
