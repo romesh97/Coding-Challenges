f8d5fb26393b4791c31efad3937716b333d633f8
Romesh <romeshperera97@gmail.com>
2022-09-12 13:15:42 +0530

    getDocumentDetails function added / document & compnay relation added

--- Challenge4/Application-Service/app/Http/Controllers/ApplicationController.php
+++ Challenge4/Application-Service/app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Document;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function getDocumentDetails(Request $request)
+    {
+        $companies = Company::all();
+        $sampleArray = [];
+        foreach ($companies as $key => $element) {
+            $document = Document::where('id', '=', $element->document_id)->first();
+            $element['companyName'] = $element->company_name;
+            $element['documentName'] = $document->document_name;
+            array_push($sampleArray, $element);
+        }
+        return $sampleArray;
+
+    }
 }
